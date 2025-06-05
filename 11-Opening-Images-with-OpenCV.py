870bc8e95c801c33d3cadb494f45c1366b8655ca
nk <141994581+kazemi1993@users.noreply.github.com>
2025-06-05 04:08:11 +0330

    11-Opening-Images-with-OpenCV.py

--- 11-Opening-Images-with-OpenCV.py
+++ 11-Opening-Images-with-OpenCV.py
@@ -1,5 +1,5 @@
 import cv2
 
-img = cv2.imread('C:/Users/kazem/Desktop/code/DATA/ai.jpg',cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('C:/Users/kazemi/Desktop/code/DATA/ai.jpg',cv2.IMREAD_GRAYSCALE)
 cv2.imshow('window_name',img)
 cv2.waitKey()
